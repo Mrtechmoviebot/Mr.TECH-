f4496053754e769db78b6f3e4b03cb3eca805208
Mrtechmoviebot <newmrtechbot@gmail.com>
2024-11-10 21:10:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ class evamaria(Client):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(os.environ.get('API_ID', '25880697'))
 API_HASH = os.environ.get('API_HASH', 'ccbeeaf507caf64c00ec327407faa7a2')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6751025808:AAH11oqeOxnziS6QDTK7byZLTwbNpAlynEw')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6751025808:AAH2By5ghcxr8CHY82LPkRg7c18SlcKgG28')
 
 
 # Bot settings
